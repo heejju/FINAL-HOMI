8a968bb8b97000ddb549e1b4781c6dee911d1a12
heeju <heeju@gmail.com>
2020-05-09 14:59:12 +0900

    friends 삭제

--- FINALHOMI/src/main/java/finalProject/homis/hobbyFarm/friends/model/exception/FriendsException.java
+++
@@ -1,8 +0,0 @@
-package finalProject.homis.hobbyFarm.friends.model.exception;
-
-public class FriendsException extends RuntimeException {
-	public FriendsException() {}
-	public FriendsException(String msg) {
-		super(msg);
-	}
-}
